9fcd4f3f8f8752555c316960e3ddfc597d2e02c9
Songren Zhao <songrenzhao@gmail.com>
2018-12-08 18:39:14 -0500

    added files

--- boa/src/stocks.js
+++ boa/src/stocks.js
@@ -12,26 +12,21 @@ class Stocks extends Component{
             lowArr: [],  
             closeArr: [],
             volumeArr: [],
-            url: "https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=AMD&interval=5min&outputsize=full&apikey=KJEQ4OXGXSCLWKKV",
+            url: null
+            // "https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=AMD&interval=5min&outputsize=full&apikey=KJEQ4OXGXSCLWKKV",
         }
     }
-    componentDidMount = () => {
-        
+    updateObeject=()=>{
         fetch(this.state.url)
-        .then(response => response.json())
-        .then(data => this.setState({
-            name: data["Meta Data"]["2. Symbol"],
-            object: data["Time Series (5min)"],
-        }))
+            .then(response => response.json())
+            .then(data => this.setState({
+                name: data["Meta Data"]["2. Symbol"],
+                object: data["Time Series (5min)"],
+            },function(){
+                this.updateArray();
+            }))
     }
-
-    display = () => {
-        let input = document.getElementById("nameInput").value;
-        this.setState({
-            url: "https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=" + input + "&interval=5min&outputsize=full&apikey=KJEQ4OXGXSCLWKKV"
-        })
-        console.log(this.state.url);
-        // let obj = this.state.object;
+    updateArray=()=>{
         const timeArr = [];
         const openArr = [];
         const highArr = [];
@@ -39,7 +34,6 @@ class Stocks extends Component{
         const closeArr = [];
         const volumeArr = [];
         Object.keys(this.state.object).forEach(key => {
-            //console.log(key, obj[key]["1. open"]);
             timeArr.push(key);
             openArr.push(this.state.object[key]["1. open"]);
             highArr.push(this.state.object[key]["2. high"]);
@@ -48,6 +42,7 @@ class Stocks extends Component{
             volumeArr.push(this.state.object[key]["5. volume"]);
         });
         timeArr.reverse(); openArr.reverse(); highArr.reverse(); lowArr.reverse(); closeArr.reverse(); volumeArr.reverse();
+        // console.log(timeArr);
         this.setState({
             timeArr: timeArr,
             openArr: openArr,
@@ -55,14 +50,31 @@ class Stocks extends Component{
             lowArr: lowArr,
             closeArr: closeArr,
             volumeArr: volumeArr,
+        },function () {
+            // console.log(this.state.object);
         });
-        console.log(timeArr, highArr);
     }
+
+    display = () => {
+        let input = document.getElementById("nameInput").value;
+        console.log(input);
+        let newUrl = "https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=" + input + "&interval=5min&outputsize=full&apikey=KJEQ4OXGXSCLWKKV";
+        this.setState({url: newUrl}, function () {
+            console.log(this.state.url);
+            this.updateObeject();
+            console.log(this.state.object);
+        });
+        console.log(this.state.object);
+        console.log(this.state.timeArr);
+    }
+
+
+
     render(){
         return(
             <div>
                 <p>Hello</p>
-                <button onClick = {this.display}>Click meeee</button>
+                <button onClick = {this.display}>Submit</button>
                 <input type = "text" id = "nameInput" placeholder = "Enter your stock symbol" ></input>
                 <LineExample
                  name = {this.state.name}
