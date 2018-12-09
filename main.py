42e2069870deaf6865dcec8965ab1a4ead84b608
Keith Low <keithlowc@gmail.com>
2018-12-09 00:37:56 -0500

    last

--- main.py
+++ main.py
@@ -13,6 +13,41 @@ def hello_world():
 	    <li>
 	    	<p>To run RNN do the following <b>url/prediction/ticker</b> - Make sure the ticker is in the yahoo finance api</p>
 	    </li>
+    	<h2>Notes:</h2>
+    	<li>
+    		<p>The request may take up to 10 seconds - So be kind and wait :)</p>
+    	</li>
+    	<li>
+    		<p>The request may time out - Please try again with a different ticker <b>AMD is known to have this issue</b></p>
+    	</li>
+    	<h2>Description:</h2>
+    	<li>
+    		<p><b>Ticker:</b> Ticker of stock</p>
+    	</li>
+    	<li>
+    		<p><b>Today:</b> Current price of stock from yahoo finance api</p>
+    	</li>
+    	<li>
+    		<p><b>Tomorrow predicted:</b> Stock price prediction of T+1</p>
+    	</li>
+    	<li>
+    		<p><b>Percentage Difference:</b> If positive value then take as gain of 1 day to another. If negative value take as loss</p>
+    	</li>
+
+    	<h2>Tested tickers - All of these are known to work!</h2>
+    	<li>
+    		<p>FB</p>
+    	</li>
+    	<li>
+    		<p>AAPL</p>
+    	</li>
+    	<li>
+    		<p>NFLX</p>
+    	</li>
+    	<li>
+    		<p>GOOGL</p>
+    	</li>
+
 	</ul>'''
 
 @app.route('/test/<ticker>', methods=['GET'])
